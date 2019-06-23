652d79a05bf6b296f09c2bb142475bb49c802888
OtiliaD <otilia.madalina.dimitriu@gmail.com>
2019-06-23 16:34:35 +0300

    curs

---
+++ index.js
@@ -0,0 +1,51 @@
+
+/*let i = 1;
+while(i <= 100){
+    console.log(i); 
+    i++;
+}*/
+
+
+/*for(var i = 1; i <= 100; i++){
+   if(i % 2 === 0){
+    console.log(i);
+   }
+}*/
+
+/*var age = Number(prompt("Please enter the age: "));
+
+switch (age) {
+  case 15:
+    alert('reducere 25%');
+    break;
+
+  case 15:
+        alert('reducere 20%');
+    break;
+
+  case 17:
+        alert('reducere 15%');
+    break;
+
+  case 18:
+        alert('reducere 10%');
+    break;
+
+  default:
+    alert('nici o reducere');
+}*/
+
+//for(var i =0; i < 10; i ++){
+//    console.log('ma execut de mai multe ori')
+//}
+
+/*var age = prompt('Please enter the age: ');
+
+if (age < 15) {
+    alert('25% reducere')
+}
+else if (age > 15 && age <25) {
+    alert('10% reducere')
+}
+    else {
+        alert('fara reducere')}*/
