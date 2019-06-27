ad11cce5ce494c0d9bd3f5a404d1ce495f4633f5
OtiliaD <otilia.madalina.dimitriu@gmail.com>
2019-06-27 23:08:53 +0300

    tema completa

---
+++ temajs2/temajs2.js
@@ -0,0 +1,51 @@
+/*function minArray(numbers) {
+  var min = numbers[0];
+  for (var i = 1; i < numbers.length; i++) {
+    if (numbers[i] < min){
+      min = numbers[i];
+    }
+   }
+ console.log(min);
+}
+minArray([5, 45, 68]);*/
+
+/*function primeNumber(n){
+  {
+    if ( n === 1 || n === 2)
+    {
+      console.log('Numarul este prim');
+    }
+    else
+    {
+      for(var x = 2; x < n; x++)
+      {
+        if(n % x === 0)
+        {
+          console.log('Numarul nu este prim');
+        }
+      }
+    }
+  }
+}
+
+primeNumber(99); */
+
+/*function helloWorldInLang(lng){
+if (lng == 'en'){
+console.log ('Hello World!');
+}
+else if (lng == 'es') {
+  console.log ('Hola Mundo!');
+}
+else if(lng == 'de') { 
+  console.log ('Hallo Welt!');
+}
+else if(lng == 'it') { 
+  console.log ('Ciao mondo!');
+}
+else{
+console.log ('Hello World!');
+}
+}
+
+helloWorldInLang('it');*/
