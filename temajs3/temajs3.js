a5b0504102bdf0d641e9a8b6b42366c790376ac1
OtiliaD <otilia.madalina.dimitriu@gmail.com>
2019-07-05 15:44:30 +0300

    tema3

---
+++ temajs3/temajs3.js
@@ -0,0 +1,34 @@
+    //var array1 = [];
+    //for(i = 0; i < 50; i++){
+    // array1.push(i);
+    //}
+    //console.log(array1);
+  
+
+//var array2 = array1.slice(0 , 9);
+//console.log(Object.getOwnPropertyNames(array2));
+
+//var newArray = array1.push({nume: "Otilia", prenume: "Dimitriu"});
+//console.log(newArray);
+
+//var array3 = Array(5).fill(5);
+//console.log(array1.concat(array3));
+
+//array1[2] = 'four';
+//console.log(array1);
+
+/*function string_to_array(str) {
+    return str.split(" ");
+};
+console.log(string_to_array("Ana NU are mere"));*/
+
+/*function truncateString(str, num) {
+    if (num <= 4) {
+    return str.slice(0, num) + "...";
+    } else if (str.length <= num){
+    return str;
+    } return str.slice(0, num + 4);
+    }
+
+console.log(truncateString("Abcdef", 4))
+console.log(truncateString("Abcdef", 8))*/
